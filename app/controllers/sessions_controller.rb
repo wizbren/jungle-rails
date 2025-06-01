71ad7ade903d62c1a88e7c6ce0dcb16c7317c026
Brendan Brown <brendan94brown@gmail.com>
2025-06-01 14:24:42 -0700

    Add login and logout actions logic to the Sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,23 @@
+
+
 class SessionsController < ApplicationController
   def new
+    #TEST should automatically render login formTEST
   end
 
   def create
+    user = User.find_by(email: params[:email])
+    if user&.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to root_path, notice: "You're now logged in."
+    else
+      flash.now[:alert] = "Invalid email or password."
+      render :new
+    end
   end
 
   def destroy
+    session[:user_id] = nil
+    redirect_to root_path, notice: "You're now logged out."
   end
 end
