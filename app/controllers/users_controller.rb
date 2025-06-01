53b2c8c0297953d640252823a11cdd1627cdb565
Brendan Brown <brendan94brown@gmail.com>
2025-06-01 14:10:25 -0700

    Add user_params method and make it private to follow Rails convention

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,23 @@
+
+
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id     #User is logged into newly created acc
+      redirect_to root_path, notice: 'Your account has been created and you are now logged in.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
   end
 end
