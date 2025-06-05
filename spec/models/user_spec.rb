d83977b4c74203e449002e9a2fb0908385cd3a38
Brendan Brown <brendan94brown@gmail.com>
2025-06-05 16:46:37 -0700

    Add test to validate new user has all required fields filled out

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,7 +4,11 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
 
   describe 'Validations' do
-
+    
+    it 'is valid with all required fields' do
+      user = User.new(first_name: 'Jane', last_name: 'Doe', email: 'jane@example.com', password: 'password', password_confirmation: 'password')
+      expect(user).to be_valid
+    end
   end
 
   describe '.authenticate_with_credentials' do
