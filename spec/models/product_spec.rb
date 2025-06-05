cf5828e4384001f6048d6a824a5da73d8d61bb01
Brendan Brown <brendan94brown@gmail.com>
2025-06-04 21:29:33 -0700

    Add first test to confirm a product is valid with all fields

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
+
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validations' do
+
+    it 'is valid with all fields present' do
+      category = Category.new(name: "Trees")
+      product = Product.new(name: "Oak", price: 100, quantity: 5, category: category)
+      expect(product).to be_valid
+    end
+  end
 end
