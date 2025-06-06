9c7df2d4422f0fa630ac84b4a7b078d34366be19
Brendan Brown <brendan94brown@gmail.com>
2025-06-05 17:39:38 -0700

    Change email validation uniqueness to be case insensitive

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@
 
 class User < ApplicationRecord
   has_secure_password
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: {case_sensitive: false}
   validates :first_name, :last_name, presence: true
 end
