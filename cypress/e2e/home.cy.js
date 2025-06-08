3c68bc639b6bbf6599fc503500d5f39692b2c49b
Brendan Brown <brendan94brown@gmail.com>
2025-06-07 17:31:48 -0700

    Add cypress test for home page product rendering

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -1,8 +1,14 @@
 
 
-/*basic home page test */
+/* basic home page test */
 describe("Home Page", () => {
   it("should visit the home page", () => {
     cy.visit("/");
+  });
+
+  /* test to confirm 2 products show up on page */
+  it("There is 2 products on the page", () => {
+    cy.visit("/");
+    cy.get(".products article").should("have.length", 2);
   });
 });
